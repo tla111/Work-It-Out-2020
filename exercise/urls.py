640caa33759439c5d466979a8ea06e87c9b407a4
Tim La <email@example.com>
2020-12-13 21:27:09 -0500

    able to change workout status from four views

--- exercise/urls.py
+++ exercise/urls.py
@@ -18,7 +18,7 @@ from django.urls import path
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from authentication.views import login_view, logout_view
 from workoutuser.views import profile_view, add_user_profile_view, update_profile_view, update_goals_view, add_todo_view, update_todo_view, remove_todo_view
-from tracker.views import dashboard_view, add_workout_view, workout_detail_view
+from tracker.views import dashboard_view, add_workout_view, workout_detail_view, new_workout_view, in_progress_workout_view, done_workout_view, invalid_workout_view
 
 urlpatterns = [
     path('', login_view, name='login'),
@@ -34,6 +34,10 @@ urlpatterns = [
     path('add_workout/', add_workout_view, name='add_workout'),
     path('workout_detail/<int:workout_id>/',
          workout_detail_view, name='workout_detail'),
+    path('new_workout/<int:workout_id>/', new_workout_view),
+    path('in_progress_workout/<int:workout_id>/', in_progress_workout_view),
+    path('done_workout/<int:workout_id>/', done_workout_view),
+    path('invalid_workout/<int:workout_id>/', invalid_workout_view),
     path('admin/', admin.site.urls),
 ]
 
--- tracker/views.py
+++ tracker/views.py
@@ -34,3 +34,35 @@ def add_workout_view(request):
 def workout_detail_view(request, workout_id):
   workout = Workout.objects.get(id=workout_id)
   return render(request, 'workout_detail.html', {'workout': workout})
+
+
+@login_required()
+def new_workout_view(request, workout_id):
+  workout = Workout.objects.get(id=workout_id)
+  workout.workout_status = 'New'
+  workout.save()
+  return redirect(f'/workout_detail/{workout_id}/')
+
+
+@login_required()
+def in_progress_workout_view(request, workout_id):
+  workout = Workout.objects.get(id=workout_id)
+  workout.workout_status = 'In Progress'
+  workout.save()
+  return redirect(f'/workout_detail/{workout_id}/')
+
+
+@login_required()
+def done_workout_view(request, workout_id):
+  workout = Workout.objects.get(id=workout_id)
+  workout.workout_status = 'Done'
+  workout.save()
+  return redirect(f'/workout_detail/{workout_id}/')
+
+
+@login_required()
+def invalid_workout_view(request, workout_id):
+  workout = Workout.objects.get(id=workout_id)
+  workout.workout_status = 'Invalid'
+  workout.save()
+  return redirect(f'/workout_detail/{workout_id}/')
