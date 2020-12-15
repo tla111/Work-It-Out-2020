31dd8e0a2b22d7afeeab529d2380fa0148d99054
Tim La <email@example.com>
2020-12-15 18:59:30 -0500

    displaying users on social page

--- exercise/urls.py
+++ exercise/urls.py
@@ -19,7 +19,7 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from authentication.views import login_view, logout_view
 from workoutuser.views import profile_view, add_user_profile_view, update_profile_view, update_goals_view, add_todo_view, update_todo_view, remove_todo_view
 from tracker.views import dashboard_view, add_workout_view, workout_detail_view, new_workout_view, in_progress_workout_view, done_workout_view, invalid_workout_view, remove_workout_view
-from social.views import social_dashboard_view
+from social.views import social_dashboard_view, author_detail_view, add_tweet_view
 
 urlpatterns = [
     path('', login_view, name='login'),
@@ -40,7 +40,9 @@ urlpatterns = [
     path('done_workout/<int:workout_id>/', done_workout_view),
     path('invalid_workout/<int:workout_id>/', invalid_workout_view),
     path('remove_workout/<int:workout_id>/', remove_workout_view),
-    path('social/', social_dashboard_view),
+    path('social/', social_dashboard_view, name='social'),
+    path('authordetail/<int:author_id>/', author_detail_view),
+    path('addmessage/', add_tweet_view),
     path('admin/', admin.site.urls),
 ]
 
--- social/views.py
+++ social/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, reverse, HttpResponseRedirect, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Message
+from .forms import AddMessageForm
 from workoutuser.models import WorkoutUser
 
 # Create your views here.
@@ -8,10 +9,40 @@ from workoutuser.models import WorkoutUser
 
 @login_required()
 def social_dashboard_view(request):
-    # logged_in_user = WorkoutUser.objects.get(username=request.user)
-    messages = Message.objects.filter(author=request.user)
-    return render(request, 'social.html', {'messages': messages})
+    workout_users = WorkoutUser.objects.all()
+    messages = Message.objects.filter(
+        author=request.user).order_by('-time_published')
+    messages_count = Message.objects.filter(author=request.user).all().count()
+    return render(request, 'social.html', {
+        'messages': messages,
+        'messages_count': messages_count,
+        'workout_users': workout_users
+    })
 
 
+@login_required()
+def author_detail_view(request, author_id):
+    author_information = WorkoutUser.objects.get(id=author_id)
+    author_messages_count = Message.objects.filter(
+        author=author_information).all().count()
+    return render(request, "author_detail.html", {
+        "author": author_information,
+        "author_messages_count": author_messages_count
+    })
 
-  
+
+@login_required()
+def add_tweet_view(request):
+    if request.method == 'POST':
+        form = AddMessageForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            new_tweet = Message.objects.create(
+                post=data['post'],
+                author=request.user
+            )
+
+            return HttpResponseRedirect(reverse('social'))
+
+    form = AddMessageForm()
+    return render(request, 'generic_form.html', {'form': form})
